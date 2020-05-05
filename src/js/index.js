852c009b131c4f82d75440b66fc41c0f82c34cf9
Artem Geroyan <geroyan.artem@mail.ru>
2020-05-05 23:10:11 +0300

    feat section tour

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
-import Post from './post'
+
 import '../styles/styles.css'
 import '../assets/rew.jpg'
 import '../styles/less.less'
-const post = new Post('webpackich');
 
-console.log('pooost323233', post.toString());
+
+
